1c1f274d46b5d0aacc0a28c58f90788f8e25ccea
a23gabli <a23gabli@student.his.se>
2024-05-20 15:20:40 +0200

    added recyclerview widget, adapter and a layout file for items

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ArrayList<RecyclerViewItem> items = new ArrayList<>(Arrays.asList(
+                new RecyclerViewItem("item1"),
+                new RecyclerViewItem("item2"),
+                new RecyclerViewItem("item3")
+        ));
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, items, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(RecyclerViewItem item) {
+                Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
